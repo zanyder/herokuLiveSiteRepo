5cd87ebe79f8fc364e47a1dc03913f7e13cec95d
Alyxander Byfield <Kandachi500@gmail.com>
2022-04-08 17:32:21 -0400

    Fixed database connectivity issues by running a tsc -watch

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SessionSecret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/contacts";
-exports.RemoteURI = "mongodb+srv://thomas:lEGPurM5wnM3fh8z@cluster0.c5n0u.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://zanyder:MNHxEKtfptZvOEVC@cluster0.rptel.mongodb.net/contacts?retryWrites=true&w=majority";
 exports.HostName = "MongoDB Altas";
 exports.SessionSecret = "WEBD6201SessionSecret";
 //# sourceMappingURL=db.js.map
